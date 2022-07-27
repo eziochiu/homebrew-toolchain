a2374fad68f9b7258f2b80061c5da3a963948244
eziochiu <eziochiuchina@hotmail.com>
2022-07-27 19:41:45 +0800

    update

--- Formula/binutils-cross-newlib.rb
+++ Formula/binutils-cross-newlib.rb
@@ -5,13 +5,11 @@ class BinutilsCrossNewlib < Formula
   sha256 "49c29e9129325e7c3b221aa829743ddcd796d024440e47c80fc0d6769af72d8a"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--enable-lite-exit",
-                          "--enable-newlib-nano-malloc",
-                          "--enable-multilib",
-                          "--disable-newlib-fvwrite-in-streami",
-                          "--enable-newlib-nano-formatted-io",
+    system "./configure", "--enable-lite-exit",
                           "--target=m68k-elf",
+                          "--disable-newlib-supplied-syscalls",
+                          "--disable-newlib-io-float",
+                          "--disable-newlib-multithread",
                           "--prefix=#{prefix}"
 
     system "make"
