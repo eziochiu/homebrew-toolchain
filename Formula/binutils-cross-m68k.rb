80dbbfe8c8fca12c801debec0cef898811172ded
eziochiu <eziochiuchina@hotmail.com>
2022-07-27 16:40:13 +0800

    update

--- Formula/binutils-cross-m68k.rb
+++ Formula/binutils-cross-m68k.rb
@@ -1,8 +1,8 @@
 class BinutilsCrossM68k < Formula
   desc "GNU Binutils for m68k cross-compiling"
   homepage "https://www.gnu.org/software/binutils/"
-  url "http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/binutils/binutils-2.37.tar.bz2"
-  sha256 "67fc1a4030d08ee877a4867d3dcab35828148f87e1fd05da6db585ed5a166bd4"
+  url "http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/binutils/binutils-2.23.1.tar.bz2"
+  sha256 "2ab2e5b03e086d12c6295f831adad46b3e1410a3a234933a2e8fac66cb2e7a19"
 
   def install
     system "./configure", "--disable-debug",
